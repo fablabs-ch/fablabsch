5637e20194f0e2e6a9c9f14254a07334ed8e2600
Boris Fritscher <boris@fritscher.ch>
2016-03-18 11:29:03 +0100

    headers

--- fablabsch/settings.py
+++ fablabsch/settings.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# FabLabs-CH a community web site for swiss fablabs
+# Copyright (C) 2016  Boris Fritscher
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 Django settings for fablabadmin project.
 """
@@ -175,7 +193,7 @@ CORS_URLS_REGEX = r'^/api/.*$'
 #
 def show_toolbar(request):
     return True
-    
+
 DEBUG_TOOLBAR_CONFIG = {
     "SHOW_TOOLBAR_CALLBACK" : show_toolbar,
 }
