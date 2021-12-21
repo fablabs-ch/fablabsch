6a1303b068fc606d7ccf6e76150e93b20bcb6c08
Boris Fritscher <boris@fritscher.ch>
2021-12-21 17:27:05 +0100

    fix import events_ics

--- backend/fablabsch/views.py
+++ backend/fablabsch/views.py
@@ -400,6 +400,7 @@ def import_spaces(request):
                 space.website = s_data['website']
                 space.facebook = s_data['facebook']
                 space.twitter = s_data['twitter']
+                space.events_ics = s_data['events_ics']
                 space.save()
 
     return HttpResponse("done")
